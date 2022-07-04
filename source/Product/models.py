25c42b4be4902aca7c699aec52ff76b5a8b7b390
Vladimir <vladimirpedenko936@gmail.com>
2022-07-04 22:58:34 +0600

    "Зарегестрировал приложение в настройках, добавил модели, зарегистрировал в админке, добывил несколько записей"

--- source/Product/models.py
+++ source/Product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [('other', 'Разное'), ('toys', 'Игрушки'), ('for home', 'Для дома'), ('present', 'Подарки'),
+                    ('in car', 'В машину'), ('hobby', 'Хобби'), ('for garden', 'Для сада')]
+
+
+class Products(models.Model):
+    product = models.CharField(max_length=100, verbose_name='Товар')
+    description = models.TextField(max_length=2000, verbose_name='Описание')
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Категории')
+    remainder = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
+
+    def __str__(self):
+        return f"{self.id}, {self.product}, {self.price}"
+
+    class Meta:
+        db_table = "Products"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
