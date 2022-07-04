4495466138d28a617d47c0222466453e573b0960
Vladimir <vladimirpedenko936@gmail.com>
2022-07-04 22:58:34 +0600

    "Зарегестрировал приложение в настройках, добавил модели, зарегистрировал в админке, добывил несколько записей, накатил миграцию для новых таблиц"

--- source/Product/admin.py
+++ source/Product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Product.models import Products
 
-# Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['product', 'category', 'remainder', 'price']
+    list_filter = ['category']
+    search_fields = ['category']
+    fields = ['product', 'description', 'category', 'remainder', 'price']
+
+
+admin.site.register(Products, ProductsAdmin)
